5590fcf778dabe3501a6706ce185d8aa391035ec
vagmr <2737165206@qq.com>
2024-05-03 00:48:52 +0800

    fixed:修复一系列问题

--- src/m3u8_downloader/m3u8_download_gui.py
+++ src/m3u8_downloader/m3u8_download_gui.py
@@ -1,6 +1,7 @@
+from email import message
 from PyQt6.QtWidgets import QApplication, QWidget, QFileDialog, QSlider
 from PyQt6.QtGui import QIcon, QDesktopServices
-from PyQt6.QtCore import QUrl
+from PyQt6.QtCore import QUrl, QTimer, QThread, pyqtSignal
 from qfluentwidgets import (InfoBar, InfoBarIcon, InfoBarPosition, MessageBox)
 from ui.ui_m3u8_dl import Ui_MainUi
 from os import makedirs, path
@@ -9,22 +10,52 @@ from random import choices
 from datetime import datetime
 import subprocess
 import sys
+import configparser
 
 
 application_path = ''
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
     application_path = sys._MEIPASS  # type: ignore
+    config_save_path = path.dirname(sys.executable)
 else:
     application_path = path.dirname(path.abspath(__file__))
+    config_save_path = application_path
+
+
+class DownloadThread(QThread):
+    """
+    下载线程类 DownloadThread
+    DownloadThread 是一个继承自 QThread 的类，用于处理下载任务。
+    它有两个信号，finished 和 error，分别在下载完成和出现错误时发出。
+
+    方法
+    - __init__(self, download_cmd): 构造函数，接受一个 download_cmd 参数，用于指定下载命令。
+    - run(self): 重写了 QThread 的 run 方法。它运行 download_cmd 命令，使用 subprocess.run() 函数。
+    如果命令成功运行，它会发出 finished 信号，并传递字符串 "下载完成"。
+    如果命令运行失败，它会发出 error 信号，并传递 CalledProcessError 异常。
+    """
+    finished = pyqtSignal(str)
+    error = pyqtSignal(Exception)
+
+    def __init__(self, download_cmd):
+        super().__init__()
+        self.download_cmd = download_cmd
+
+    def run(self):
+        try:
+            subprocess.run(self.download_cmd, shell=True, check=True)
+            self.finished.emit("下载完成")
+        except subprocess.CalledProcessError as e:
+            self.error.emit(e)
 
 
 class M3u8DownloadGui(QWidget, Ui_MainUi):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
         self.setupUi(self)
-        self.__init__download_dir()
-        self._init_window()
         self._init_variables()
+        self._init_window()
+        self.read_config()
         self._init_output_name()
         self.signalToSlot()
 
@@ -33,14 +64,57 @@ class M3u8DownloadGui(QWidget, Ui_MainUi):
 
         self.download_url = ""
         self.output_name = ""
+        self.output_path = ""
         self.download_threads = 6
         self.CaptionLabel_2.setText(
             str(self.download_threads)
         )
+        self.config = configparser.ConfigParser()
+
+    def closeEvent(self, event):
+        """
+        重写closeEvent方法，实现窗口关闭时执行一些操作
+        如果保存路径发生变化，则保存路径信息到配置文件中
+        """
+        config_file = path.join(config_save_path, 'settings.ini')
+        self.config.read(config_file)
+        old_output_path = self.config.get(
+            section="Settings", option="output_path")
+        if old_output_path != self.output_path:
+            if self.is_confirm(content="检测到下载目录发生变化，是否保存?"):
+                self.write_config()
+        event.accept()
+
+    def read_config(self):
+        """
+        读取配置文件中的下载目录信息，并设置到文本框中
+        如果不存在配置文件，则创建配置文件
+        """
+        config_file = path.join(config_save_path, 'settings.ini')
+        if path.exists(config_file):
+            self.config.read(config_file)
+            self.output_path = self.config.get(
+                'Settings', 'output_path', fallback=self.output_path)
+            self.LineEdit_2.setText(self.output_path)
+        else:
+            self.__init__download_dir()
+            self.write_config()
+
+    def write_config(self):
+        """
+        保存下载目录信息到配置文件中
+        """
+
+        self.config['Settings'] = {'output_path': self.output_path}
+        config_file = path.join(config_save_path, 'settings.ini')
+        with open(config_file, 'w') as f:
+            self.config.write(f)
 
     # 初始化窗口
+
     def _init_window(self):
         self.setWindowIcon(QIcon("../assets/1.ico"))
+        self.setFixedSize(600, 400)
         self.LineEdit_3.setClearButtonEnabled(True)
         self.Slider.setMinimum(3)
         self.Slider.setMaximum(16)
@@ -52,9 +126,10 @@ class M3u8DownloadGui(QWidget, Ui_MainUi):
 
     # 初始化下载目录
     def __init__download_dir(self):
-        self.output_path = path.join(application_path, 'download')
+        print("初始化下载目录")
+        self.output_path = path.join(config_save_path, 'download')
         if not path.exists(self.output_path):
-            makedirs(path.join(application_path, 'download'))
+            makedirs(path.join(config_save_path, 'download'))
         self.LineEdit_2.setText(self.output_path)
 
     def show_info(self, content='', title='', parent=None, success=True, duration=2000):
@@ -135,26 +210,28 @@ class M3u8DownloadGui(QWidget, Ui_MainUi):
                 success=False,
             )
             return
-        print("开始下载")
-        exc_path = path.join(application_path, 'lib/m3u8Lib.exe')
-        download_cmd = f' {exc_path} "{self.download_url}"   --save-dir {self.output_path} --save-name {self.output_name}  --thread-count {self.download_threads}'
-        try:
-            subprocess.run(download_cmd, shell=True, check=True)
-            self.show_info(
-                content=f"下载成功，文件保存至：{self.output_path}",
-                parent=self,
-                success=True)
-            if self.is_confirm(content="是否打开下载目录?"):
-                QDesktopServices.openUrl(QUrl.fromLocalFile(self.output_path))
 
-        except subprocess.CalledProcessError as e:
-            self.show_info(
-                content=f"下载失败，错误信息：{e.stderr.decode()}",
-                parent=self,
-                success=False,
-                duration=3000,
-            )
-            return
+        self.show_info(
+            content=f"开始下载,线程数{self.download_threads}，请查看控制台输出", parent=self, success=True)
+
+        exc_path = path.join(application_path, 'lib/m3u8Lib.exe')
+        download_cmd = f'{exc_path} "{self.download_url}" --save-dir "{self.output_path}" --save-name {self.output_name} --thread-count {self.download_threads}'
+
+        message = f"下载完成, 文件路径：{self.output_path}"
+        self.download_thread = DownloadThread(download_cmd)
+        self.download_thread.finished.connect(
+            lambda: self.on_download_finished(message))
+        self.download_thread.error.connect(self.on_download_error)
+        self.download_thread.start()
+
+    def on_download_finished(self, message):
+        print(message)
+        if self.is_confirm(content="是否打开下载目录?"):
+            QDesktopServices.openUrl(QUrl.fromLocalFile(self.output_path))
+
+    def on_download_error(self, exception):
+        self.show_info(content=f"下载失败，错误信息：{exception.stderr.decode()}",
+                       parent=self, success=False, duration=3000)
 
     def signalToSlot(self):
         """ 信号与槽的绑定
